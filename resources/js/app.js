a0daebf59a2195f788b48a88e74a2f2e259367b1
Dieter <dburgstaller@gmx.de>
2023-01-26 06:31:19 +0100

    Master Laravel 9, Vue3 &Inertia Fullstack 2023 Styling  Error Messages

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,17 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import Mainlayout from '@/Layouts/MainLayout.vue';
 import { ZiggyVue } from 'ziggy';
+import { InertiaProgress } from '@inertiajs/progress';
 import '../css/app.css';
+
+InertiaProgress.init({
+  delay: 0,
+  color: '#29d',
+  includeCSS: true,
+  showSpinner: true,
+
+})
+
 createInertiaApp({
   resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`,  import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
